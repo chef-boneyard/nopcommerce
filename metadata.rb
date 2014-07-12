f5db3e946ad12ffce80e81437c5e7e32f28cf2b3
Julian C. Dunn <jdunn@getchef.com>
2014-07-12 00:11:02 -0400

    Update metadata for release of 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'nopcommerce'
-maintainer       'Opscode, Inc.'
-maintainer_email 'jdunn@opscode.com'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'jdunn@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures nopCommerce, the shopping cart software for ASP.NET'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
